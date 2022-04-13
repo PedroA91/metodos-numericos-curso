1ee39d9a30b4e2e64132f151382509d7f6ecdc9e
PedroA91 <pedrinto91@gmail.com>
2022-04-12 21:57:38 -0500

    pedro

--- PYTHON/run_function.py
+++ PYTHON/run_function.py
@@ -2,12 +2,13 @@ import time
 import numpy as np
 import mn_AlgebraLineal
 
-N = 4
+N = 256
 a = np.random.rand(N,N)
 b = np.random.rand(N,1)
 
 t_ini = time.time()
-P,L,U = mn_AlgebraLineal.PLU(a)
+X = mn_AlgebraLineal.DownGauss(a,b)
+print(X)
 t_fin = time.time()
 
 print("El tiempo de computacion fue de ",t_fin-t_ini)
